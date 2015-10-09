d948ceff37f9a46345beeb76fe2e8a6ad63c92b8
Maru Newby <marun@redhat.com>
2015-10-08 18:36:51 -0700

    Fix mirroring to target different path on linux

--- fedora/mirror-repos/run.sh
+++ fedora/mirror-repos/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+MIRROR_PATH=/Users/Shared/yum
+if [ ! -d "${MIRROR_PATH}" ]; then
+  MIRROR_PATH=/opt/data/yum
+fi
+
 tag=fedora/localmirror
 docker build -t ${tag} .
-docker run -dt -v /Users/Shared/yum:/opt/yum ${tag}
+docker run -dt -v ${MIRROR_PATH}:/opt/yum ${tag}
