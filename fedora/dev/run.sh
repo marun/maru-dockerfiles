f4cdd199b5245132308b241266880c54429fca80
Maru Newby <marun@redhat.com>
2016-04-05 15:39:47 +0000

    Add dockerfile for dev env

---
+++ fedora/dev/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p /tmp/openshift-dind-cluster
+docker run -d -p 7022:22 -v /opt/src:/opt/src -v /opt/data:/opt/data\
+ --name dev -h dev -v /var/run/docker.sock:/var/run/docker.sock\
+ -v /tmp/openshift-dind-cluster:/tmp/openshift-dind-cluster\
+ marun/dev /usr/sbin/sshd -D
