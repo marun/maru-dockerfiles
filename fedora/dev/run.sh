2043adba5149934618702202627ed1fb87140675
Maru Newby <marun@redhat.com>
2016-05-06 20:49:03 +0000

    Always restart dev container

--- fedora/dev/run.sh
+++ fedora/dev/run.sh
@@ -4,4 +4,5 @@ mkdir -p /tmp/openshift-dind-cluster
 docker run -d -p 7022:22 -v /opt/src:/opt/src -v /opt/data:/opt/data\
  --name dev -h dev -v /var/run/docker.sock:/var/run/docker.sock\
  -v /tmp/openshift-dind-cluster:/tmp/openshift-dind-cluster\
+ --restart=always\
  marun/dev /usr/sbin/sshd -D
