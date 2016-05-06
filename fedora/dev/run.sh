aba88598aabe34380c71e28e0ac1185223d3ea8e
Maru Newby <marun@redhat.com>
2016-05-06 20:49:33 +0000

    Ensure dev container mounts test path

--- fedora/dev/run.sh
+++ fedora/dev/run.sh
@@ -4,5 +4,6 @@ mkdir -p /tmp/openshift-dind-cluster
 docker run -d -p 7022:22 -v /opt/src:/opt/src -v /opt/data:/opt/data\
  --name dev -h dev -v /var/run/docker.sock:/var/run/docker.sock\
  -v /tmp/openshift-dind-cluster:/tmp/openshift-dind-cluster\
+ -v /tmp/openshift:/tmp/openshift\
  --restart=always\
  marun/dev /usr/sbin/sshd -D
