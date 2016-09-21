63e3bcda09dff8c63d61af7fdf754bd5dc7bba30
Maru Newby <marun@redhat.com>
2016-09-21 13:13:26 -0700

    Set timezone in fedora dev container

--- fedora/dev/run.sh
+++ fedora/dev/run.sh
@@ -5,5 +5,6 @@ docker run -d -p 7022:22 -v /opt/src:/opt/src -v /opt/data:/opt/data\
  --name dev -h dev -v /var/run/docker.sock:/var/run/docker.sock\
  -v /tmp/openshift-dind-cluster:/tmp/openshift-dind-cluster\
  -v /tmp/openshift:/tmp/openshift\
+ -e TZ="${TZ-`readlink /etc/localtime | sed -e 's,.*/usr/share/zoneinfo/,,'`}"\
  --restart=always\
  marun/dev
