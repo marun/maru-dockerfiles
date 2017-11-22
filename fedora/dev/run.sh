bf660f8bea643ed5b257956627bc55481a364213
Maru Newby <marun@redhat.com>
2017-11-22 14:32:49 -0800

    Install delve in fedora dev image

--- fedora/dev/run.sh
+++ fedora/dev/run.sh
@@ -9,5 +9,6 @@ docker run -d -p 7022:22 -v /opt/src:/opt/src -v /opt/data:/opt/data\
  --name dev -h dev -v /var/run/docker.sock:/var/run/docker.sock\
  -v /tmp/openshift-dind-cluster:/tmp/openshift-dind-cluster\
  -v /tmp/openshift:/tmp/openshift\
+ --security-opt=seccomp:unconfined\
  --restart=always\
  maru/dev:fedora27
