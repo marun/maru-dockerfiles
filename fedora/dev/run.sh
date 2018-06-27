50a60e1b31d20f17dd2fdc0eb0bb0f5eca52d065
Maru Newby <marun@redhat.com>
2018-06-27 18:04:47 +0000

    Mount .kube and .minikube in dev image
    This allows kubectl calls to minikube in the host from the dev
    container.

--- fedora/dev/run.sh
+++ fedora/dev/run.sh
@@ -9,6 +9,8 @@ docker run -d -p 7022:22 -v /opt/src:/opt/src -v /opt/data:/opt/data\
  --name dev -h dev -v /var/run/docker.sock:/var/run/docker.sock\
  -v /tmp/openshift-dind-cluster:/tmp/openshift-dind-cluster\
  -v /tmp/openshift:/tmp/openshift\
+ -v /home/maru/.kube:/home/dev/.kube\
+ -v /home/maru/.minikube:/home/maru/.minikube\
  --security-opt=seccomp:unconfined\
  --restart=always\
  maru/dev:fedora27
